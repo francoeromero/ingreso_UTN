0b09b9e0c7bd5b656fdd7141dfabc2f9264ac25e
franromeroe <franromeroeze@gmail.com>
2022-08-22 17:26:12 -0300

    Ejercicios terminados

---
+++ jsEntradaSalida-02.js
@@ -0,0 +1,9 @@
+/*
+Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+	let nombre = prompt('Cual es tu nombre?')
+	alert('Bienvenido ' + nombre);
+}
+

c133dfb971046e5abb8da954553c3b82c35b8bf7
franromeroe <franromeroeze@gmail.com>
2022-11-11 16:13:28 -0300

    JS

---
+++ jsEntradaSalida-04.js
@@ -0,0 +1,11 @@
+/* 
+Debemos lograr tomar un dato por 'PROMPT' 
+y lo muestro por 'ID' al presionar el botón 'mostrar'
+*/
+function mostrar()
+{
+	let nombre = prompt('hola, cual es tu nombre?');
+	txtIdNombre.value = nombre;
+
+}
+

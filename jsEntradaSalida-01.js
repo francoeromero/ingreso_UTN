70fef687c4ad5689893b2f4e60041fc94e9bfcf4
franromeroe <franromeroeze@gmail.com>
2022-11-29 18:12:08 -0300

    utn

--- jsEntradaSalida-01.js
+++ jsEntradaSalida-01.js
@@ -12,4 +12,16 @@ alert genera una ventana emergente
 Cuales son los datos de entrada de la funcion alert? 
 rta: el texto entre parentesis ( ) estan para recibir los datos de la funcion para cumplir la sentencia 
 
+como crear una funcion?
+1 paso: Declarar una funcion
+2 paso: Darle un nombre
+3 paso: Parametros 
+4 paso: Instrucciones de la funcion
+5 paso: Llamar ala funcion para activar 
+
+
+function nombreFunction (parametros) {
+	intrucciones
+}
+nombreFunction(); 
 */

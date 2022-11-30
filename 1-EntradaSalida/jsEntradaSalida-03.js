3878fb6d801d64eb482227bac0841c15fd47fda2
franromeroe <franromeroeze@gmail.com>
2022-11-30 13:48:05 -0300

    utn

---
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -0,0 +1,14 @@
+/*
+Debemos lograr tomar un dato por 'ID'
+y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+	var nombreIngresado;
+	nombreIngresado=txtIdNombre.value;
+	alert(nombreIngresado);
+
+}
+
+/*
+APUNTES:
+*/
---
+++ 1-EntradaSalida/jsEntradaSalida-05.js
@@ -0,0 +1,13 @@
+/*
+Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
+ej.: "Usted se llama José y tiene 66 años" 	*/
+function mostrar()
+{	
+	let nombre = txtIdNombre.value;
+	let edad = txtIdEdad.value;
+	alert("Usted se llama " + nombre + " y tiene " + edad);
+}
+
+/*
+APUNTES:
+*/

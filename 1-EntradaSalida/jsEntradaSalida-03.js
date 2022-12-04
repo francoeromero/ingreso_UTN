2d995068a640a72414f0d3b89fc3a064a8d80589
franromeroe <franromeroeze@gmail.com>
2022-12-04 07:12:40 -0300

    utn

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -23,4 +23,11 @@ y lo que hace es asociar un numero a un caracter
 
 string "1" number 1 (no confundir)
 
+Si yo quiero capturar la informacion que el usuario coloca en un input de html, tengo que ir al JS y hacer esto:
+
+1- Creamos una variable (estamos pidiendo un espacio de memoria)
+2- asignamos el valor que se coloque llamando al id del input y poniendo ".value"
+3- Ese valor tiene que meterse en el espacio de memoria que creamos que seria la variable entonces ponemos variable = infoinput.value; 
+
+
 */

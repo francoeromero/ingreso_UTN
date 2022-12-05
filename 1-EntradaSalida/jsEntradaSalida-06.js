5d2fbcc5dea2831661857631b13a10f32b5804b7
franromeroe <franromeroeze@gmail.com>
2022-12-05 08:07:11 -0300

    utn

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -8,6 +8,26 @@ function sumar()
 	// let dos = txtIdNumeroDos.value;
 	// alert(uno + dos);
 
+	//pido espacio de memoria
+	let num1;
+	let num2;
+	let resultado;
+
+	//paso de string a number el valor de la entrada de las dos cadenas
+	num1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	num2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+
+	//hago la operacion sumando los dos valores numericos
+	resultado = num1 + num2;
+
+	//muestro el resultado en pantalla
+	alert("El resultado es " + resultado);
+
+	//limpio las entradas para una nueva operacion		
+	document.getElementById("txtIdNumeroUno").value = "";
+	document.getElementById("txtIdNumeroDos").value = "";
+
+
 }
 
 /*
@@ -90,7 +110,43 @@ primero opera y luego asigna a x
 ParseInt y ParseFloat
 
 let x = "20";
+let y = "30";
+let z;
+
+convertimos de string a numer
 x = parseInt(x);
+y = parseInt(y);
+
+opera y asigno a z
+z = x + y;
+
+muestro el resultado en pantalla
+alert(z);
+
+paseInt lee de caracter a caracter verificando si es un tipo de texto Number, si no es numerico te muestra solo lo que leyo que era un numero ej:
+12juan ---> 12 
+
+Si quisiera pasar numeros con signos osea numeros especificos por ejemplo 3.14, parseInt solo te mostrara el 3 para que te muestre todo el numero usamos parseFloat()
+
+
+entonces agregamos el parseInt() a los input para que te devuelva el resultado de una operacion numerica
+
+	let num1;
+	let num2;
+	let resultado;
+	
+	num1 = document.getElemenbyId("txtNumeroUno").value;
+
+	parseInt(num1);
+
+	num2 = document.getElemenbyId("txtNumeroDos").value;
+
+	parseInt(num2);
+
+	resultado = num1 + num2;
+
+	alert("la suma es " resultado);
+
 
 
 */

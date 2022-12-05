ecb4f8ae1ce4cfd208c507bc35630ab7a570d04a
franromeroe <franromeroeze@gmail.com>
2022-12-05 07:05:17 -0300

    utn

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,8 +4,38 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	let uno = txtIdNumeroUno.value;
-	let dos = txtIdNumeroDos.value;
-	alert(uno + dos);
+	// let uno = txtIdNumeroUno.value;
+	// let dos = txtIdNumeroDos.value;
+	// alert(uno + dos);
+
 }
 
+/*
+APUNTES:
+
+se suman
+
+	let x = 30;
+	let y = 20;
+	let z;
+
+	z = x + y;
+
+	alert(z);
+
+no se suman
+
+	let x = "30";
+	let y = 20;
+	let z;
+
+	z = x + y;
+
+	alert(z);
+
+	string + string = string 
+	string + number = string
+	number + string = string
+	number + number = number
+
+*/

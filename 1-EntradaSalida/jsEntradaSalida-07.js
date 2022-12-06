8ea58d94c1a8515ca752f4271d7abfb3af004563
franromeroe <franromeroeze@gmail.com>
2022-12-06 07:22:43 -0300

    utn

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -17,21 +17,78 @@ function sumar()
 	//Ahora hago la operacion y asigno el valor a la variable resultado
 	resultado = num1 + num2;
 
+	//muestro el valor en pantalla
+
+	alert("El resultado es " + resultado);
+
+	//limpio las entradas para una nueva operacion
+	document.getElementById("txtIdNumeroUno").value = "";
+	document.getElementById("txtIdNumeroDos").value = "";
+
 
 }
 
 function restar()
 {
-	
+	//pido espacio de memoria
+	let num1;
+	let num2;
+	let resultado;
+
+	//Asigno el valor de las entradas alas variables y cambio de string a number
+	num1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	num2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+
+	//operacion
+	resultado = num1 - num2;
+
+	//muestro en resultado en la pantalla
+	alert("El resultado es " + resultado)
+
+	//limpio las entradas para una nueva operacion
+	document.getElementById("txtIdNumeroUno").value = "";
+	document.getElementById("txtIdNumeroDos").value = "";
+
 }
 
 function multiplicar()
 { 
+	let num1;
+	let num2;
+	let resultado;
+
+	//Asigno el valor de las entradas alas variables y cambio de string a number
+	num1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	num2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+
+	//operacion
+	resultado = num1 * num2;
+
+	//muestro en resultado en la pantalla
+	alert("El resultado es " + resultado)
+	//limpio las entradas para una nueva operacion
+	document.getElementById("txtIdNumeroUno").value = "";
+	document.getElementById("txtIdNumeroDos").value = "";
 	
 }
 
 function dividir()
 {
-	
+	let num1;
+	let num2;
+	let resultado;
+
+	//Asigno el valor de las entradas alas variables y cambio de string a number
+	num1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	num2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+
+	//operacion
+	resultado = num1 / num2;
+
+	//muestro en resultado en la pantalla
+	alert("El resultado es " + resultado)
+	//limpio las entradas para una nueva operacion
+	document.getElementById("txtIdNumeroUno").value = "";
+	document.getElementById("txtIdNumeroDos").value = "";
 }
 

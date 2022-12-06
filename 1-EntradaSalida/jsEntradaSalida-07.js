f680eb986a925f8a540c240126e254f534d055db
franromeroe <franromeroeze@gmail.com>
2022-12-06 07:12:07 -0300

    utn

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -10,7 +10,14 @@ function sumar()
 	let num2;
 	let resultado;
 
-	
+	//asigno el valor alas variables y transformo de string a number
+	num1 = parseInt(document.getElementById("txtIdNumeroUno").value);
+	num2 = parseInt(document.getElementById("txtIdNumeroDos").value);
+
+	//Ahora hago la operacion y asigno el valor a la variable resultado
+	resultado = num1 + num2;
+
+
 }
 
 function restar()

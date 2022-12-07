c40ebd5d6ef4c304f8977e7c58d7ed8072fdf4c8
franromeroe <franromeroeze@gmail.com>
2022-12-07 06:53:48 -0300

    utn

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -6,5 +6,30 @@ en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
 
-	alert("ok");
+	// let sueldo;
+	// let resultado;
+
+	// sueldo = parseInt(document.getElementById("txtIdSueldo").value);
+
+	// resultado = 10 * sueldo / 100;
+	
+	// resultado = document.getElementById("txtIdResultado").value;
+	// alert("El resultado es " + resultado);
+
+	let sueldo;
+	let resultado;
+	//paso el valor a la variable sueldo y cambio de string a number
+	sueldo = parseInt(document.getElementById("txtIdSueldo").value);
+	//hago la operacion y lo meto en la variable aumento
+	resultado =  10 * sueldo / 100;
+	
+
+	resultado = parseInt(document.getElementById("txtIdResultado").value);
+	
 }
+
+/*
+APUNTES:
+
+
+*/

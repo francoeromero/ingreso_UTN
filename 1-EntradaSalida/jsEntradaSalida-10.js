d9d9db818e2b69cc75bb1f8b7af00ed04ad11294
franromeroe <franromeroeze@gmail.com>
2022-12-07 08:35:35 -0300

    utn

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -5,11 +5,23 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	alert("ok");
+	//reservo espacio de memoria
+	let importe;
+	let descuento;
+	let nuevoimporte;
+	//transformo el importe en valor number de la entrada txtidimporte
+	importe = parseInt(document.getElementById("txtIdImporte").value);
+	//operacion
+	descuento = .25 * importe;
+	nuevoimporte = importe - descuento;
+	//luego muestro en pantalla el resultado
+	document.getElementById("txtIdResultado").value = nuevoimporte;
+
 }
 
 /*
 APUNTES: 
+
 
 
 

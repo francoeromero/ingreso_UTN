d5584ea87b01234a4dda76b479d34e3769347345
franromeroe <franromeroeze@gmail.com>
2022-12-08 13:53:50 -0300

    utn

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -5,19 +5,20 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
+    //pido espacio de memoria creando variables
 	let preciouno;
     let preciodos;
     let preciotres;
     let resultado;
-
+    //ahora cambio las entradas a tipo number y dentro tengo que mencionar que el VALOR se cambio de string a number 
     preciouno = parseInt(document.getElementById("txtIdPrecioUno").value);
     preciodos = parseInt(document.getElementById("txtIdPrecioDos").value);
     preciotres = parseInt(document.getElementById("txtIdPrecioTres").value);
-
+    //ahora hago la operacion de la sumatoria y asignarlo a otra variable
     resultado = preciouno + preciodos + preciotres;
-
+    //luego mostrarlo en pantalla usando el alert
     alert("La suma es de " + resultado);
-
+    //luego limpiamos las entradas para una nueva operacion
     document.getElementById("txtIdPrecioTres").value = "";
     document.getElementById("txtIdPrecioDos").value = "";
     document.getElementById("txtIdPrecioUno").value = "";

94249e0a439a345555d47ace57001da07060d324
franromeroe <franromeroeze@gmail.com>
2022-12-12 12:38:25 -0300

    utn

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -2,6 +2,7 @@
 A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
+
 */
 function Sumar () 
 {

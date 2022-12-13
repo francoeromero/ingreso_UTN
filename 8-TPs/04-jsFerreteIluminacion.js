9697832d91c96bb60d3bd68d0db4265c18466221
franromeroe <franromeroeze@gmail.com>
2022-12-13 20:01:26 -0300

    utn

--- 8-TPs/04-jsFerreteIluminacion.js
+++ 8-TPs/04-jsFerreteIluminacion.js
@@ -1,5 +1,6 @@
 /*4.	Para el departamento de iluminación:
 Tomando en cuenta que todas las lámparas están en oferta al mismo precio de $35 pesos final.
+
 A.	Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%. 
 
 B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" se hace un descuento del 40 % y si es de otra marca el descuento es del 30%.

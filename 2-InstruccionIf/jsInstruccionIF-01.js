02a44a5319e641ab5023155d7c21e9cbc368f299
franromeroe <franromeroeze@gmail.com>
2022-12-15 06:24:25 -0300

    utn

--- 2-InstruccionIf/jsInstruccionIF-01.js
+++ 2-InstruccionIf/jsInstruccionIF-01.js
@@ -1,6 +1,18 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad;
+	if(edad == 15){
+		
+		alert("Niña bonita");
+	} else {
+		alert("mayor de edad")
+	}
 
 }//FIN DE LA FUNCIÓN
+
+/*
+Condicional son operadores booleanos
+
+1) declaro una variable para pedir espacio de memoria
+2) setenencia if, y la condicional sea una declaracion con una variable
+*/

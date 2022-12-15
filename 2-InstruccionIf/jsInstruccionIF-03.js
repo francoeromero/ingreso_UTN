431ceb3059c390576ab1730487ad71df4fc74d92
franromeroe <franromeroeze@gmail.com>
2022-12-15 07:33:43 -0300

    utn2

--- 2-InstruccionIf/jsInstruccionIF-03.js
+++ 2-InstruccionIf/jsInstruccionIF-03.js
@@ -1,6 +1,16 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	//pido espacio de memoria
+	let edad;
+
+	//asigno al input
+	edad = parseInt(document.getElementById("txtIdEdad").value);
+
+	//condicional 
+	if (edad >= 18){
+		alert("Sos mayor de edad");
+	} else {
+		alert("Sos menor de edad");
+	}
 
 }//FIN DE LA FUNCIÓN

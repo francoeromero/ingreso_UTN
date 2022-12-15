8111a05af9c3f71b038e588048250c54c7108121
franromeroe <franromeroeze@gmail.com>
2022-12-15 19:36:55 -0300

    utn

--- 2-InstruccionIf/jsInstruccionIF-07.js
+++ 2-InstruccionIf/jsInstruccionIF-07.js
@@ -7,13 +7,49 @@ function mostrar()
 	edad = parseInt(document.getElementById("txtIdEdad").value);
 	estadocivil = document.getElementById("estadoCivil").value;
 
-	if(edad < 18 || estadocivil == value("casado") || estadocivil == value("divorciado") ){
-		alert("Es muy pequeño para NO ser soltero");
+	if(edad < 18  && estadocivil != "Soltero"){
+		alert("Es muy pequeño para NO ser soltero.");
+	}
+	else {
+		alert ( "ok");
+	}
+	
+	
+}
 
-	} else if (estadocivil == value("soltero")) {
-		alert("Usted es mayor de edad");
+/*
+
+solucion 1:
+
+if(estadocivil == "Casado" || estadocivil == "Divorciado" || edad <= 17){
+		alert("Es muy pequeño para NO ser soltero.");
+	}
+	else if ( edad >= 17  && estadocivil == "Casado"){
+		alert("Adulto");
+	}
+
+correccion: Al marcar estadocivil como casado y divorciado, lo que sucede que es si alguien es mayor de edad y es casado o divorciado iba a saltar la misma alerta entonces la solucion es colocar un desigual !=  
+
+pero la pregunta si quisiera NO poner el desigual como lo haria ? 
+
+solucion :
+
+if(edad < 18  &&  estadocivil != "Soltero"){
+		alert("Es muy pequeño para NO ser soltero.");
+	}
+	else {
+		alert ( "ok");
 	}
 	
 
+	
+Analisis del enunciado:
+"edad menor a 18 años" y "un estado civil DISTINTO a soltero"
 
-}//FIN DE LA FUNCIÓN
+edad menor a 18 años ---->  edad < 18
+un estado civil DISTINTO a soltero ---- >   estadocivil != "Soltero"
+
+
+if ( condicion1 + operador logico + condicion2)
+
+*/

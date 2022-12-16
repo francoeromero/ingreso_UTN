3fcc1aff12b28d8aaace8850d792d7d2cdc91b93
franromeroe <franromeroeze@gmail.com>
2022-12-16 20:22:00 -0300

    utn

--- 3-InstruccionSwitch/jsInstruccionSwitch-03.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-03.js
@@ -1,10 +1,26 @@
 function mostrar()
 {
 	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	let mes = document.getElementById("txtIdMes").value; 
+
+
+	switch (mes){
+		case "Febrero" :
+			alert("Este mes no tiene mas de 29 dias");
+			break;
+		default:
+		alert("Este mes tiene 30 o mas dias");
+		break;
+	}
+
+	// if (mes == "Febrero"){
+	// 	alert("Este mes no tiene mas de 29 dias");
+	// }
+	// else {
+	// 	alert("Este mes tiene 30 o mas dias");
+	// }
 	
 	
 
 
-}//FIN DE LA FUNCIÓN
+}

e6e9f16e6ea29d22449a3bd863f48e227758f94d
franromeroe <franromeroeze@gmail.com>
2022-12-16 20:44:58 -0300

    utn

--- 3-InstruccionSwitch/jsInstruccionSwitch-03.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-03.js
@@ -8,7 +8,8 @@ function mostrar()
 		case "Febrero" :
 			alert("Este mes no tiene mas de 29 dias");
 			break;
-		default:
+
+		default: // es como el else
 		alert("Este mes tiene 30 o mas dias");
 		break;
 	}
--- 3-InstruccionSwitch/jsInstruccionSwitch-04.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-04.js
@@ -1,8 +1,56 @@
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	let mes = document.getElementById("txtIdMes").value; 
+
+	switch (mes){
+		case "Enero":
+			alert("tiene 30 días");
+			break;
+
+		case "Febrero":
+			alert("tiene 28 días");
+			break;
+
+		case "Marzo":
+			alert("tiene 31 días");
+			break;
+
+		case "Abril":
+			alert("tiene 30 días");
+			break;
+
+		case "Mayo":
+			alert("tiene 31 días");
+			break;
+
+		case "Junio":
+			alert("tiene 30 días");
+			break;
+
+		case "Julio":
+			alert("tiene 31 días");
+			break;
+
+		case "Agosto":
+			alert("tiene 31 días");
+			break;
+
+		case "Septiembre":
+			alert("tiene 30 días");
+			break;
+			
+		case "Octubre":
+			alert("tiene 31 días");
+			break;
+
+		case "Noviembre":
+			alert("tiene 30 días");
+			break;
+
+		case "Diciembre":
+			alert("tiene 31 días");
+			break;
+	}
 	
 	
 

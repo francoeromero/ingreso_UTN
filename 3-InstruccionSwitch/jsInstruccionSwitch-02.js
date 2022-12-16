d2eb3da19c019963471411a29c15b5a49f7ca33c
franromeroe <franromeroeze@gmail.com>
2022-12-16 20:10:29 -0300

    utn

--- 3-InstruccionSwitch/jsInstruccionSwitch-02.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-02.js
@@ -1,10 +1,20 @@
 function mostrar()
 {
 	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	let mes = document.getElementById("txtIdMes").value; // var mesDelAño =txtIdMes.value;
+	
+	switch (mes){
+		case "Marzo" :
+			alert("Abrigate que hace frio");
+		break;
 
+		case "Mayo" :
+			alert("Falta para el invierno");
+		break;
 
+		case "Septiembre" :
+			alert("Ya pasamos el frio, ahora calor!!!.");
+		break;
 
-
-}//FIN DE LA FUNCIÓN
+	}
+}

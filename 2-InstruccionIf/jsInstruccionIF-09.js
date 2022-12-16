3a1edc678cb315b68998a06edb1c921bd9d0cf5c
franromeroe <franromeroeze@gmail.com>
2022-12-15 22:19:56 -0300

    utn

--- 2-InstruccionIf/jsInstruccionIF-09.js
+++ 2-InstruccionIf/jsInstruccionIF-09.js
@@ -1,5 +1,22 @@
 function mostrar()
 {
-	
+	let numero;
+    let maximo = 10;
+    let minimo = 1;
+
+    numero = Math.round(Math.random() * (maximo - minimo) + minimo);
+
+    alert(numero);
 
 }
+
+/*
+Math.random()  [0 , 1) va  del 0 al 1   valores decimales aleatorios 
+
+Math.round  =   para redondear a numeros enteros  es como parseInt pero la diferencia que parseInt transforma el valor de string a number enteros
+
+hacemos una ecuacion con el math.random utilizando variables con valores numericos y todo dentro del math.round para que redondee el numero en entero
+
+
+
+*/
--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,33 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
+	let nota;
 
-}//FIN DE LA FUNCIÓN
+	nota = Math.round(Math.random() * 9 + 1);
+
+	if (nota >= 9) {
+		alert("EXCELENTE: " + nota);
+	}
+	else if (nota < 4) {
+		alert("Vamos, la proxima se puede!: " + nota);
+	}
+	else {
+		alert("APROBÓ: " + nota);
+	}
+}
+
+/*
+solucion !: 
+
+
+	if (nota >= 9) {
+		alert("EXCELENTE " + nota);
+	}
+	else if (nota >= 4) {
+		alert("APROBÓ " + nota);
+	}
+	else if (nota >= 1){
+		alert("Vamos, la proxima se puede! " + nota);
+	}
+
+
+*/

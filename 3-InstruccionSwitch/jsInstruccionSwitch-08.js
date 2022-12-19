081cd715045f8eafe2d06d2b62e26bcb0166fb19
franromeroe <franromeroeze@gmail.com>
2022-12-19 18:11:04 -0300

    utn

--- 3-InstruccionSwitch/jsInstruccionSwitch-08.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-08.js
@@ -1,6 +1,19 @@
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
-
+	let txtIdDestino = document.getElementById("txtIdDestino").value;
+	
+	switch (txtIdDestino) {
+		case "Bariloche":
+			alert("En Bariloche hace FRIO");
+			break;
+		case "Cataratas":
+			alert("En Cataratas hace CALOR");
+			break;
+		case "Mar del plata":
+			alert("En Mar del plata hace CALOR");
+			break;
+		case "Ushuaia":
+			alert("En Ushuaia hace FRIO");
+			break;
+	}
 }

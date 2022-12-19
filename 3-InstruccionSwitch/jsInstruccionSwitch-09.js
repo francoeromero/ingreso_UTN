6950faf7010ba105678fe33366ab61d64787cfca
franromeroe <franromeroeze@gmail.com>
2022-12-19 18:19:07 -0300

    utn2

--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -3,7 +3,41 @@ function mostrar()
 	let txtIdEstacion = document.getElementById("txtIdEstacion").value;
 	let txtIdDestino = document.getElementById("txtIdDestino").value;
 	
+	switch (txtIdEstacion){
+		case "Invierno":
+			alert("");
+			break;
 
-	
+		case "Verano":
+			alert("");
+			break;
+
+		case "Otoño":
+			alert("");
+			break;
+
+		case "Primavera":
+			alert("");
+			break;
+	}
+
+	switch(txtIdDestino){
+		case "Bariloche":
+			alert("");
+			break;
+
+		case "Cataratas":
+			alert("");
+			break;
+
+		case "Mar del plata":
+			alert("");
+			break;
+
+		case "Cordoba":
+			alert("");
+			break;
+	}
+
 
 }

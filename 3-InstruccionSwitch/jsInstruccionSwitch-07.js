cdac29df137a4371b652b48ff71bc106cc29dd6d
franromeroe <franromeroeze@gmail.com>
2022-12-19 18:04:18 -0300

    utn

--- 3-InstruccionSwitch/jsInstruccionSwitch-07.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-07.js
@@ -1,6 +1,20 @@
 function mostrar()
 {
-	var destinoIngresado =txtIdDestino.value;
-	alert(destinoIngresado);
+	let txtIdDestino = document.getElementById("txtIdDestino").value;
+	
+	switch (txtIdDestino) {
+		case "Bariloche":
+			alert("Bariloche tiene 41°09′S 71°18′O﻿ de coordenadas");
+			break;
+		case "Cataratas":
+			alert("Cataratas tiene 25°41′43″S 54°26′12″O de coordenadas");
+			break;
+		case "Mar del plata":
+		alert("Mar del plata tiene 38°00′S 57°33′O de coordenadas");
+		break;
+		case "Ushuaia":
+			alert("Ushuaia tiene 54°48′58″S 68°19′25″O de coordenas");
+			break;
+	}
 
-}//FIN DE LA FUNCIÓN
+}

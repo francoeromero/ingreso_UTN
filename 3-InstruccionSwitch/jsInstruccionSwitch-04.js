6ff9c412e92e355b78cc79fb1de68a30c2ccd743
franromeroe <franromeroeze@gmail.com>
2022-12-22 20:35:36 -0300

    utn

--- 3-InstruccionSwitch/jsInstruccionSwitch-04.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-04.js
@@ -2,55 +2,86 @@ function mostrar()
 {
 	let mes = document.getElementById("txtIdMes").value; 
 
-	switch (mes){
+
+	switch(mes){
 		case "Enero":
-			alert("tiene 30 días");
+		case "Abril":
+		case "Junio":
+		case "Septiembre":
+		case "Noviembre":
+			alert("Tiene 30 dias");
 			break;
 
 		case "Febrero":
-			alert("tiene 28 días");
+			alert("Tiene 28 dias");
 			break;
 
-		case "Marzo":
-			alert("tiene 31 días");
+		default:
+			alert("Tiene 31 dias");
 			break;
 
-		case "Abril":
-			alert("tiene 30 días");
-			break;
+		// case "Marzo":
+		// case "Mayo":
+		// case "Julio":
+		// case "Agosto":
+		// case "Octubre":
+		// case "Diciembre":
+		// 	alert("Tiene 31 dias");
+		// 	break;
 
-		case "Mayo":
-			alert("tiene 31 días");
-			break;
+	
 
-		case "Junio":
-			alert("tiene 30 días");
-			break;
-
-		case "Julio":
-			alert("tiene 31 días");
-			break;
-
-		case "Agosto":
-			alert("tiene 31 días");
-			break;
-
-		case "Septiembre":
-			alert("tiene 30 días");
-			break;
-			
-		case "Octubre":
-			alert("tiene 31 días");
-			break;
-
-		case "Noviembre":
-			alert("tiene 30 días");
-			break;
-
-		case "Diciembre":
-			alert("tiene 31 días");
-			break;
 	}
+
+	// switch (mes){
+	// 	case "Enero":
+	// 		alert("tiene 30 días");
+	// 		break;
+
+	// 	case "Febrero":
+	// 		alert("tiene 28 días");
+	// 		break;
+
+	// 	case "Marzo":
+	// 		alert("tiene 31 días");
+	// 		break;
+
+	// 	case "Abril":
+	// 		alert("tiene 30 días");
+	// 		break;
+
+	// 	case "Mayo":
+	// 		alert("tiene 31 días");
+	// 		break;
+
+	// 	case "Junio":
+	// 		alert("tiene 30 días");
+	// 		break;
+
+	// 	case "Julio":
+	// 		alert("tiene 31 días");
+	// 		break;
+
+	// 	case "Agosto":
+	// 		alert("tiene 31 días");
+	// 		break;
+
+	// 	case "Septiembre":
+	// 		alert("tiene 30 días");
+	// 		break;
+			
+	// 	case "Octubre":
+	// 		alert("tiene 31 días");
+	// 		break;
+
+	// 	case "Noviembre":
+	// 		alert("tiene 30 días");
+	// 		break;
+
+	// 	case "Diciembre":
+	// 		alert("tiene 31 días");
+	// 		break;
+	// }
 	
 	
 

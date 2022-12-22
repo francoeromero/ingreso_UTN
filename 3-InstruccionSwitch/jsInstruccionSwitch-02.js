17e61b3a067086fd6e6a5d4c95d8baebe0ff8199
franromeroe <franromeroeze@gmail.com>
2022-12-22 20:10:01 -0300

    utn

--- 3-InstruccionSwitch/jsInstruccionSwitch-02.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-02.js
@@ -4,43 +4,26 @@ function mostrar()
 	let mes = document.getElementById("txtIdMes").value; // var mesDelAño =txtIdMes.value;
 	
 	switch (mes){
-		case "Enero":
-			alert("Ya pasamos el frio, ahora calor!!!.");
+		case "Marzo" :
+		case "Abril":
+		case "Mayo" :
+			alert("Falta para el invierno.");
 			break;
+
+		case "Junio":
+		case "Julio":
+		case "Agosto":
+			alert("Abrigate que hace frio.");
+			break;
+
+		case "Septiembre" :
+		case "Octubre" :
+		case "Noviembre" :
+		case "Diciembre" :
+		case "Enero":
 		case "Febrero":
 			alert("Ya pasamos el frio, ahora calor!!!.");
 			break;
-		case "Marzo" :
-			alert("Abrigate que hace frio");
-		break;
-		case "Abril":
-			alert("Abrigate que hace frio");
-			break;
-		case "Mayo" :
-			alert("Falta para el invierno");
-		break;
-		case "Junio":
-			alert("Falta para el invierno");
-			break;
-		case "Julio":
-			alert("invierno");
-			break;
-		case "Agosto":
-			alert("invierno");
-			break;	
-		case "Septiembre" :
-			alert("Ya pasamos el frio, ahora calor!!!.");
-		break;
-		case "Octubre" :
-			alert("Ya pasamos el frio, ahora calor!!!.");
-		break;
-		case "Noviembre" :
-			alert("Ya pasamos el frio, ahora calor!!!.");
-		break;
-		case "Diciembre" :
-			alert("Ya pasamos el frio, ahora calor!!!.");
-		break;
-
 	}
 }
 

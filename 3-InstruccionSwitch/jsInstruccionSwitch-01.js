8710cc24f48191bef1db3400d9d77748d097e20d
franromeroe <franromeroeze@gmail.com>
2022-12-22 17:26:31 -0300

    utn

--- 3-InstruccionSwitch/jsInstruccionSwitch-01.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-01.js
@@ -38,6 +38,14 @@ function mostrar()
 }
 
 /*
+
+LA DIFERENCIA ENTRE SWITCH E IF
+
+Como puedes ver, la declaración switch es más concisa y fácil de leer, especialmente cuando hay un gran número de condiciones que verificar. Sin embargo, las declaraciones if son más flexibles y pueden manejar una gama más amplia de condiciones.
+
+Es importante elegir la declaración de control adecuada para el trabajo. En general, switch es una buena opción cuando hay un número limitado de valores discretos que puede tomar una expresión, y if es una buena opción cuando se tienen condiciones más complejas que no se pueden expresar fácilmente con una declaración switch.
+
+
 Como vemos en el codigo estamos evaluando a la variable mes para condicionarlo en diferentes bloques, en situaciones como esta vamos a usar SWITCH
 
 "En el caso que la variables "variable" hago esto" y voy a tener casos (case)

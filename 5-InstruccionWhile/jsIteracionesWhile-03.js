f5912ba92df3bddd78c993f2cf8614d016ecfd7c
franromeroe <franromeroeze@gmail.com>
2022-12-24 18:31:36 -0300

    utn

--- 5-InstruccionWhile/jsIteracionesWhile-03.js
+++ 5-InstruccionWhile/jsIteracionesWhile-03.js
@@ -2,16 +2,17 @@
 al presionar el botón pedir la CLAVE (ayuda: es utn750)*/
 function mostrar()
 {
-	let claveIngresada;
-	claveIngresada = prompt("ingrese el número clave.");
+	let claveIngresada; // pido espacio de memoria
+	claveIngresada = prompt("ingrese el número clave."); //le pido al usuario que ingrese la clave para meterlo a esa variable
 	
-	const CLAVECORRECTA = "utn750";
+	const CLAVECORRECTA = "utn750"; //creo una variable constante para la clave correcta
 
-	while(claveIngresada != CLAVECORRECTA){
+	//uso while para generar un bucle, de no ser correcta la clave entonces que la vuelva a escribir
+	while(claveIngresada != CLAVECORRECTA){ 
 		alert("INCORRECTO");
 		claveIngresada = prompt("vuelva a ingresar");
 	}
 
-	alert("CORRECTO!!!!")
+	alert("CORRECTO!!!!");
 	
 }

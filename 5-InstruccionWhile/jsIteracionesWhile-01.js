e2acbeff67f22446d5e8edbb4869656b74550e7a
franromeroe <franromeroeze@gmail.com>
2022-12-24 15:45:21 -0300

    utn

--- 5-InstruccionWhile/jsIteracionesWhile-01.js
+++ 5-InstruccionWhile/jsIteracionesWhile-01.js
@@ -11,10 +11,14 @@ SENTENCIAS SECUENCIALES
 
 Las sentencias secuenciales son aquellas que se ejecutan en orden, una detrás de otra. Es decir, se ejecutan de manera secuencial, de la primera a la última.
 
-INSTRUCCIONES CONDICIONALES 
+SENTENCIAS CONDICIONALES (TRUE OR FALSE)
 
+Las sentencias condicionales son aquellas que se ejecutan solo si se cumple cierta condición. Por ejemplo, una sentencia "if" (si) permite ejecutar un bloque de código solo si se cumple una determinada condición.
 
 INSTRUCCIONES ITERATIVAS  O REPETITIVAS 
+
+Las sentencias iterativas, también conocidas como bucles o ciclos, permiten ejecutar un bloque de código varias veces de manera consecutiva. Por ejemplo, un bucle "for" (para) permite ejecutar un bloque de código un número determinado de veces.
+
 
 
 */

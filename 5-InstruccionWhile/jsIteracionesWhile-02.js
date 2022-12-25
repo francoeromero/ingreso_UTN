4c2a51f1afdd8a524e22efced10b35e3ae4eda53
franromeroe <franromeroeze@gmail.com>
2022-12-25 10:49:25 -0300

    utn

--- 5-InstruccionWhile/jsIteracionesWhile-02.js
+++ 5-InstruccionWhile/jsIteracionesWhile-02.js
@@ -9,5 +9,4 @@ function mostrar()
 		alert(i);
 		i--;
 	} 
-
 }
--- 5-InstruccionWhile/jsIteracionesWhile-04.js
+++ 5-InstruccionWhile/jsIteracionesWhile-04.js
@@ -3,7 +3,11 @@ al presionar el botón
 pedir un número entre 0 y 9 inclusive.*/
 function mostrar()
 {
-	var numeroIngresado;
+let numeroIngresado;
 	numeroIngresado = prompt("ingrese un número entre 0 y 10.");
 	
-}//FIN DE LA FUNCIÓN
+}
+
+/*
+
+*/

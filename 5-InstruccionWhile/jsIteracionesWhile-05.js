ca8577c2273227773c4af4ab68982e1527a7cf19
franromeroe <franromeroeze@gmail.com>
2022-12-26 12:52:22 -0300

    utn

--- 5-InstruccionWhile/jsIteracionesWhile-05.js
+++ 5-InstruccionWhile/jsIteracionesWhile-05.js
@@ -3,10 +3,12 @@ Al presionar el botón pedir un sexo
 'f' para femenino, 'm' para masculino.*/
 function mostrar()
 {
-	var sexoIngresado;
+	let sexoIngresado;
 	sexoIngresado = prompt("ingrese f ó m .");
+	sexoIngresado = parseInt(document.getElementById("txtIdSexo").value);
+
+	
 
 
-
-	txtIdSexo.value=sexoIngresado;
-}//FIN DE LA FUNCIÓN
+	
+}

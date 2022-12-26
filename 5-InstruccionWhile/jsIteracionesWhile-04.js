dc6ae5e5c4985e04ab3c895194fdc28d250c6c92
franromeroe <franromeroeze@gmail.com>
2022-12-26 12:50:26 -0300

    utn

--- 5-InstruccionWhile/jsIteracionesWhile-04.js
+++ 5-InstruccionWhile/jsIteracionesWhile-04.js
@@ -4,7 +4,13 @@ pedir un número entre 0 y 9 inclusive.*/
 function mostrar()
 {
 let numeroIngresado;
-	numeroIngresado = prompt("ingrese un número entre 0 y 10.");
+	numeroIngresado = prompt("ingrese un número entre 0 y 9.");
+
+	while(numeroIngresado >= 9 || numeroIngresado <= 0){
+		alert("El numero tiene que ser entre 0 y 9");
+		numeroIngresado = prompt("vuelva a escribir"); //error que cometi es que me olvide renombrar la variable numeroIngresado
+	}
+	alert("numero correcto!!");
 	
 }
 

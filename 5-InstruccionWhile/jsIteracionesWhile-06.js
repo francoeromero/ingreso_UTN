fae2590ed37b7fd67a4eabcab2e06c52ec8edab5
franromeroe <franromeroeze@gmail.com>
2022-12-27 21:50:01 -0300

    utn

--- 5-InstruccionWhile/jsIteracionesWhile-06.js
+++ 5-InstruccionWhile/jsIteracionesWhile-06.js
@@ -1,12 +1,42 @@
 function mostrar()
 {
-	let contador;
-	let acumulador;
-	let numeroIngresado;
+	//primero pido espacio de memoria creando las variables de los 5 numeros, la suma y el promedio
+	let num1;
+	let num2;
+	let num3;
+	let num4;
+	let num5;
+	let suma;
+	let promedio;
 
-	contador=0;
-	acumulador=0;
+	//luego creamos un prompt para que el usuario ingrese los numeros, le damos paseint porque quiero que sean numeros enteros
+	num1 = parseInt( prompt("Ingrese numero: "));
+	num2 = parseInt( prompt("Ingrese numero: "));
+	num3 = parseInt( prompt("Ingrese numero: "));
+	num4 = parseInt( prompt("Ingrese numero: "));
+	num5 = parseInt( prompt("Ingrese numero: "));
 	
-	document.getElementById("txtIdSuma").value = acumulador;
-	document.getElementById("txtIdPromedio").value = acumulador/5;
+	//ahora hacemos el calculo de los numeros que puso el usuario
+	suma = num1 + num2 + num3 + num4 + num5;
+	promedio = suma / 5;
+	
+
+	//por ultimo arrojamos el resultado en las entradas
+	document.getElementById("txtIdSuma").value = suma;
+	document.getElementById("txtIdPromedio").value = promedio;
+
 }
+
+/*
+
+1- Creamos las variables (espacios de memoria)
+
+2- pedimos al usuario que llene de informacion a esas variables
+
+3- hacemos el calculo con la informacion que puso el usuario
+
+4- mostramos el resultado 
+
+
+
+*/

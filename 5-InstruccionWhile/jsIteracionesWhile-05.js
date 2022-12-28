bbee387eb64d0df33f532857cf2defae04cbc3bb
franromeroe <franromeroeze@gmail.com>
2022-12-27 21:54:23 -0300

    utn

--- 5-InstruccionWhile/jsIteracionesWhile-05.js
+++ 5-InstruccionWhile/jsIteracionesWhile-05.js
@@ -2,19 +2,31 @@
 Al presionar el botón pedir un sexo
 'f' para femenino, 'm' para masculino.*/
 function mostrar()
-{
-	let sexoIngresado = prompt("ingrese f ó m");
-	
+{	
+	//pedimos espacio de memoria
+	let sexoIngresado;
+	//pedimos al usuario que coloque informacion dentro del espacio de memoria osea la variable
+	sexoIngresado = prompt("ingrese f ó m");
 
-	
+	//calculo con lo que el usuario dejo de dato
+
 	while(sexoIngresado == "f" || sexoIngresado == "m"){
+		//muestro el resultado de lo que puso con una alerta si fue correcto
 	 alert("sexo elegido");
 	 sexoIngresado = prompt("vuelve a pedir otro sexo");
 	}
-
+		//muestro el resultado de lo que puso con una alerta si fue incorrecto
 	alert("incorrecto");
-
-
 	
 }
+
+/*
+siempre es el mismo mecanismo:
+
+1. pido espacio de memoria 
+2. solicito informacion al usuario
+3. con esa info calculo
+4 muestro el resultado 
+
+*/
 

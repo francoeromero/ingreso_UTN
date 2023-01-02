d0c1ac0da7d28e428f6a549ed6cc0b87b24c4a7f
franromeroe <franromeroeze@gmail.com>
2023-01-02 08:27:12 -0300

    utn

--- 5-InstruccionWhile/jsIteracionesWhile-07.js
+++ 5-InstruccionWhile/jsIteracionesWhile-07.js
@@ -2,34 +2,59 @@
 Al presionar el botón pedir  números  hasta que el USUARIO QUIERA 
 e informar la suma acumulada y el promedio.
 */
-function mostrar()
-{
-	//creamos los espacios de memoria
+// function mostrar()
+// {
+// 	//creamos los espacios de memoria
+// 	let num = 0; // cero como valor inicial 
+// 	let promedio; 
+// 	let acumulador = 0; 
+// 	let i = 0;
+	
+// 	//calculo segun los numeros que puso el usuario
+	
+// 	while(num < 5){  // 5 vueltas del bucle
+// 		num = parseInt(prompt("Ingrese el numero : ")); // pido al usuario un valor numerico 
+
+// 		acumulador = acumulador + num; // el acumulador acumula los valores que coloca el usuario para no ser perdidos durante el bucle
+
+// 		i++; //contador  cuenta las veces que va girando el bucle
+// 	}
+
+// 	promedio = acumulador / 5; // y luego de las 5 vuelta, el valor del acumulador lo divido por 5 para dar un promedio
+
+// 	//muestro el resultado en las entradas
+// 	document.getElementById("txtIdSuma").value = acumulador;
+// 	document.getElementById("txtIdPromedio").value = promedio;
+
+	
+// }
+
+function mostrar() {
 	let num = 0;
-	let promedio;
-	let acumulador = 0;
 	let i = 0;
-	
-	//calculo segun los numeros que puso el usuario
-	
-	while(num < 5){  // 5 vueltas del bucle
-		num = parseInt(prompt("Ingrese el numero : ")); // pido al usuario un valor numerico 
+	let acumulador = 0;
+	let promedio ; 
 
-		acumulador = acumulador + num; // el acumulador acumula los valores que coloca el usuario para no ser perdidos durante el bucle
+	while( i < 3) 
+	{
+		num = parseInt(prompt("ingresa un numero: "));
 
-		i++; //contador  cuenta las veces que va girando el bucle
+		acumulador = acumulador + num; // el acumulador es una variable que se aparta del bucle para recordar el num anterior y sumarlo al que viene num + num + num ... 
+
+		i++; // el contador, una variable auxiliar que ayuda a controlar las repeticiones del bucle 
 	}
 
-	promedio = acumulador / 5; // y luego de las 5 vuelta, el valor del acumulador lo divido por 5 para dar un promedio
+		promedio = acumulador / 5;
 
-	//muestro el resultado en las entradas
-	document.getElementById("txtIdSuma").value = acumulador;
-	document.getElementById("txtIdPromedio").value = promedio;
+		document.getElementById("txtIdSuma").value = acumulador;
+		document.getElementById("txtIdPromedio").value = promedio;
 
 	
 }
 
 /*
+
+.....
 let numero;
 let i = 0;
 

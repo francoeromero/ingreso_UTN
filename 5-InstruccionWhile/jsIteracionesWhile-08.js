6463e341e8bf6e6d56be46e69f5521fa645ace57
franromeroe <franromeroeze@gmail.com>
2023-01-04 12:09:56 -0300

    if dentro de while

--- 5-InstruccionWhile/jsIteracionesWhile-08.js
+++ 5-InstruccionWhile/jsIteracionesWhile-08.js
@@ -26,19 +26,39 @@ function mostrar()
 
 		num = parseInt(prompt("Ingrese un número: "));
 
-		sumapositivos = num
+		if (num >= 0) {
+			sumapositivos = sumapositivos + num;
+		} else {
+			multnegativos = multnegativos * num;
+		}
 
 		preguntar = prompt("Quieres escribir otro numero?");
 
-		i = i + 1;
+		// i = i + 1;
 
 	}while (preguntar == 's');
 
-	document.getElementById("txtIdSuma").value = num ;
-	// document.getElementById("txtIdProducto").value = ;
+	document.getElementById("txtIdSuma").value = sumapositivos ;
+	 document.getElementById("txtIdProducto").value = multnegativos ;
 
 }
+/*
+Programa sin codigos (su estructura) : 
 
+//	declaracion de las variables
+
+//	debemos armar un bucle del tipo mientras el usuario quiera (do while)
+
+// pedir los numeros ---> dentro del bucle
+
+// analizar el signo de cada numero (si es positivo o negativo) ---> dentro del bucle
+
+//	acumulamos el numero en el acumulador correspondiente  ---> dentro del bucle
+
+//	mostrar los resultados ---> fuera del bucle
+
+
+*/
 
 /*
 ESTRUCTURA

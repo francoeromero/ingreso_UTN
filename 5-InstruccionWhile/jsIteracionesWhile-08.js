f9a3cb9ce667cc79ce537e6abbdec966807bf055
franromeroe <franromeroeze@gmail.com>
2023-01-04 12:38:53 -0300

    if dentro de while

--- 5-InstruccionWhile/jsIteracionesWhile-08.js
+++ 5-InstruccionWhile/jsIteracionesWhile-08.js
@@ -3,22 +3,9 @@ Al presionar el botón pedir  números  hasta que el usuario quiera,
 sumar los que son positivos y multiplicar los negativos.*/
 function mostrar()
 {
-	// var contador;
-	// var respuesta;
-	// var sumaPositivos;
-	// var multiplicacionNegativos;
-	// contador=0;
-	// sumaPositivos=0;
-	// multiplicacionNegativos=1;
-	// respuesta='si';
-
-
-	// txtIdSuma.value=sumaPositivos;
-	// txtIdProducto.value=multiplicacionNegativos;
 	
-	let num = 0;
-	let i = 0;
-	let preguntar;
+	let num = 0; // el num es la variable que guarda los numeros del usuario
+	let preguntar; // para activar el bucle
 	let sumapositivos = 0; //aca pongo 0 como inicializacion porque se suman 
 	let multnegativos = 1; // puse 1 como inicializacion porque si pondria 0, al multiplicarse con cualquier num daria 0 siempre
 
@@ -26,23 +13,28 @@ function mostrar()
 
 		num = parseInt(prompt("Ingrese un número: ")); // le pido que ingrese numero al usuario
 
-		if (num >= 0) {  // utilizo if si es mayor o igual a 0 , que se acumule sumandose 	
-			sumapositivos = sumapositivos + num;
-		} else {
-			multnegativos = multnegativos * num; // de ser negativo entonces que se multiplique
-		}
+			if (num >= 0) {  // utilizo if si es mayor o igual a 0 , que se acumule sumandose 	
+				sumapositivos = sumapositivos + num;
+			} else {
+				multnegativos = multnegativos * num; // de ser negativo entonces que se multiplique
+			}
 
 		preguntar = prompt("Quieres escribir otro numero?"); // ahora le pregunto al usuario si quiere que agrege otro numero
 
 
 	}while (preguntar == 's'); // de poner 's' que representa el si, entonces que continue el bucle 
 
-	
- // muestro en pantalla los resultados 
+
+		 // muestro en pantalla los resultados 
 	document.getElementById("txtIdSuma").value = sumapositivos ;
 	document.getElementById("txtIdProducto").value = multnegativos ;
 
 }
+
+
+
+
+
 /*
 Programa sin codigos (su estructura) : 
 
@@ -57,6 +49,32 @@ Programa sin codigos (su estructura) :
 //	acumulamos el numero en el acumulador correspondiente  ---> dentro del bucle
 
 //	mostrar los resultados ---> fuera del bucle
+
+
+*/
+
+/*
+otra forma de resolverlo:
+
+let num;
+let positivos = 0;
+let negativos = 1; 
+let seguir;
+
+do {
+if (num >= 0){
+	positivos = positivos + num;
+} else {
+	negativos = negativos * num;
+}
+
+seguir = prompt("quieres escribir otro numero: ");
+
+} while (seguir == 's');
+
+	document.getElementById("txtIdSuma").value = positivos ;
+	document.getElementById("txtIdProducto").value = negativos ;
+
 
 
 */

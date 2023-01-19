6ab06feffbdfa696f33a5fbc4f67d9d694a9d23f
franromeroe <franromeroeze@gmail.com>
2023-01-19 20:03:27 -0300

    piedra papel o tijera

--- 8-TPs/07-jsPiedarPapelTijera1.js
+++ 8-TPs/07-jsPiedarPapelTijera1.js
@@ -8,22 +8,39 @@ var eleccionMaquina;
 
 function comenzar()
 {
-	
-
-
+	 numSecreto = Math.floor(Math.random() * (3 - 1 + 1)) + 1;//sin let porque es un ambito global
+   
 }//FIN DE LA FUNCIÓN
 function piedra()
 {
-	
+    if(numSecreto == 1){
+        alert(`Usted eligio PIEDRA y su contrincante eligió PIEDRA, empate!`);
+    }else if (numSecreto == 2 ){
+        alert(`Usted eligio PIEDRA y su contrincante eligió PAPEL, perdiste!`);
+    }else {
+        alert(`Usted eligio PIEDRA y su contrincante eligió TIJERA, ganaste!!`)
+    }
 
 }//FIN DE LA FUNCIÓN
 function papel()
 {
-
+    if(numSecreto == 1){
+        alert(`Usted eligio PAPEL y su contrincante eligió PIEDRA, ganaste!`);
+    }else if (numSecreto == 2 ){
+        alert(`Usted eligio PAPEL y su contrincante eligió PAPEL, empate!`);
+    }else {
+        alert(`Usted eligio PAPEL y su contrincante eligió TIJERA, perdiste!!`)
+    }
 
 }//FIN DE LA FUNCIÓN
 function tijera()
 {
-	
+	if(numSecreto == 1){
+        alert(`Usted eligio TIJERA y su contrincante eligió PIEDRA, perdiste!`);
+    }else if (numSecreto == 2 ){
+        alert(`Usted eligio TIJERA y su contrincante eligió PAPEL, ganaste!`);
+    }else {
+        alert(`Usted eligio TIJERA y su contrincante eligió TIJERA, empate!!`)
+    }
 
 }//FIN DE LA FUNCIÓN

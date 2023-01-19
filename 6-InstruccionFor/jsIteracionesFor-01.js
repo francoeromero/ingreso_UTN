1a629aca6aea152c51b49dcc2fdb0ec8a065a217
franromeroe <franromeroeze@gmail.com>
2023-01-18 22:39:40 -0300

    adivina numero

--- 6-InstruccionFor/jsIteracionesFor-01.js
+++ 6-InstruccionFor/jsIteracionesFor-01.js
@@ -1,4 +1,4 @@
 function mostrar()
 {
-	alert("ok");
+	
 }
--- 8-TPs/05-jsAdivinaElNumero1.js
+++ 8-TPs/05-jsAdivinaElNumero1.js
@@ -7,20 +7,40 @@ secreto del 1 al 100, en la pantalla del juego
 “Usted es un ganador!!! y en solo X intentos”.
 de no ser igual se debe informar si “falta…”  para llegar al número secreto  o si “se pasó…”  del número secreto.
 */
-var numeroSecreto; 
-var contadorIntentos;
+const comenzar = () =>{
+  numSecreto = Math.floor(Math.random() * (100 - 1 + 1)) + 1; //ambito global sin let o const
+  
+}
 
-function comenzar()
-{
-	//Genero el número RANDOM entre 1 y 100
-	 
-		//alert(numeroSecreto );
-	
+const verificar = () =>{
+  
+  let num = parseInt(document.getElementById("txtIdNumero").value);
+  
+  if(num === numSecreto){
+    alert (`su numero es ${num} y el numero secreto es ${numSecreto} USTED ES EL GANADOR`);
+  }
+  else{
+    alert(`su numero es  ${num} y el numero secreto es ${numSecreto} PERDIÓ`)
+  }
+
+  document.getElementById("txtIdNumero").value = ""; 
 
 }
 
-function verificar()
-{
-	
-	
-}
+/*
+__________________________________
+FORMULA PARA GENERAR UN RANGO DE NUMEROS ESPECIFICOS RANDOMS
+
+max y min son los limites del rango de numero por ejemplo entre 3 y 9 
+
+Math.floor(Math.random() * (max - min + 1)) + min;
+____________________________________
+OTRA FORMULA 
+max es el numero limite, por ejemplo quiero que me de numeros randoms hasta el 20, entonces (20-1) = 19, estaria multiplicando por 19 
+
+Math.floor(Math.random() * (max-1));
+____________________________________
+Math.floor() se utiliza para redondear hacia abajo un número decimal a su entero más cercano, mientras que Math.random() se utiliza para generar un número aleatorio decimal entre 0 y 1.
+
+*/
+

26ac24dd2f19f0386d5014e15d36175470837a6f
franromeroe <franromeroeze@gmail.com>
2023-01-19 21:35:35 -0300

    adivina el numero 2.0

--- 8-TPs/06-jsAdivinaElNumero2.js
+++ 8-TPs/06-jsAdivinaElNumero2.js
@@ -15,14 +15,71 @@ var contadorIntentos;
 function comenzar()
 {
 	//Genero el número RANDOM entre 1 y 100
-	 
+	numSecreto = Math.floor(Math.random() * (100 - 1 + 1)) + 1; //ambito global sin let o const
 	
 
 }
 
 function verificar()
 {
-	
+  let i = 0;
+  let a = 0;
+  let seguir; 
+  let num;
+  let flag = 0;
+  
+  do {
+    if (flag == 0){   
+      num = parseInt(document.getElementById("txtIdNumero").value);
+      if(num === numSecreto){
+        alert (`su numero es ${num} y el numero secreto es ${numSecreto} USTED ES EL GANADOR`);
+      }
+      else{
+        alert(`su numero es  ${num} y el numero secreto es ${numSecreto} PERDIÓ`)
+      }
+      
+      a = a + i;
+      i++;
+      flag = 1;
+    }
+  num = parseInt(prompt("Ingrese un numero del 1 al 100"));
+  
+  if(num === numSecreto){
+    alert (`su numero es ${num} y el numero secreto es ${numSecreto} USTED ES EL GANADOR`);
+  }
+  else{
+    alert(`su numero es  ${num} y el numero secreto es ${numSecreto} PERDIÓ`)
+  }
+  
+  a = a + i;
+  i++;
+  
+  seguir = prompt("Quieres seguir intentando?");
+  }while(seguir == 's');
+
+  if(i == 1){
+	alert("Usted es Psiquico");
+  }
+  else if (i == 2){
+	alert("Excelente percepción")
+  }
+  else if(i == 3){
+	alert("Esto es suerte!")
+  }
+  else if(i == 4){
+	alert("Excelente tecnica")
+  }
+  else if(i ==5){
+	alert("usted esta en la media")
+  }
+  else {
+	alert("falta tecnica")
+  }
+
+  document.getElementById("txtIdIntentos").value = i;
+  document.getElementById("txtIdNumero").value = num;
+
+ 
 	
 
 }

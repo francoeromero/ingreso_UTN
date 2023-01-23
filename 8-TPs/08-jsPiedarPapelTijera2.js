46d87c90f8706fc60c4de5b082c5319602b66750
franromeroe <franromeroeze@gmail.com>
2023-01-23 17:58:31 -0300

    utn

--- 8-TPs/08-jsPiedarPapelTijera2.js
+++ 8-TPs/08-jsPiedarPapelTijera2.js
@@ -1,31 +1,64 @@
-var eleccionMaquina;
-var ContadorDeEmpates=0;
-var ContadorDeGanadas=0;
-var ContadorDePerdidas=0;
+let eleccionMaquina;
+let ContadorDeEmpates=0;
+let ContadorDeGanadas=0;
+let ContadorDePerdidas=0;
 
-function comenzar()
-{
-	//Genero el número RANDOM entre 1 y 3
-	 	
+// Funcion para generar un numero random que sera la eleccion de la maquina
+const comenzar = () => {
+  eleccionMaquina = Math.floor(Math.random() * 3);
+}
 
+// Funcion para cuando se elige piedra
+const piedra = () => {
+  if (eleccionMaquina === 0) {
+    ContadorDeEmpates++; // aumenta el contador de empates
+    alert("Empate!");
+  }
+  else if (eleccionMaquina === 1) {
+    ContadorDePerdidas++;// aumenta en perdidas
+    alert("Perdiste!");
+  }
+  else {
+    ContadorDeGanadas++;//aumenta en ganadas
+    alert("Ganaste!");
+  }
+  actualizarContadores();// llamo a la funcion que deposita los valores a las entradas
+}
 
+const papel = () => {
+  if (eleccionMaquina === 0) {
+    ContadorDeGanadas++;
+    alert("Ganaste!");
+  }
+  else if (eleccionMaquina === 1) {
+    ContadorDeEmpates++;
+    alert("Empate!");
+  }
+  else {
+    ContadorDePerdidas++;
+    alert("Perdiste!");
+  }
+  actualizarContadores();
+}
 
-}//FIN DE LA FUNCIÓN
-function piedra()
-{
-	
+const tijera = () => {
+  if (eleccionMaquina === 0) {
+    ContadorDePerdidas++;
+    alert("Perdiste!");
+  }
+  else if (eleccionMaquina === 1) {
+    ContadorDeGanadas++;
+    alert("Ganaste!");
+  }
+  else {
+    ContadorDeEmpates++;
+    alert("Empate!");
+  }
+  actualizarContadores();
+}
 
-}//FIN DE LA FUNCIÓN
-function papel()
-{
-
-}//FIN DE LA FUNCIÓN
-function tijera()
-{
-	
-}//FIN DE LA FUNCIÓN
-
-function mostrarResultado()
-{
-
+const actualizarContadores = () => {
+  document.getElementById("txtIdGanadas").value = ContadorDeGanadas;
+  document.getElementById("txtIdPerdidas").value = ContadorDePerdidas;
+  document.getElementById("txtIdEmpatadas").value = ContadorDeEmpates;
 }

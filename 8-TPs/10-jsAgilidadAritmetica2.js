ec43eab04a6e4cf4fb22fd083174b3436b137c25
franromeroe <franromeroeze@gmail.com>
2023-01-24 13:58:49 -0300

    utn

--- 8-TPs/10-jsAgilidadAritmetica2.js
+++ 8-TPs/10-jsAgilidadAritmetica2.js
@@ -3,15 +3,41 @@ se agregara un temporizador que
  a los cinco segundos dará por terminado 
  el juego  de no ser ingresado el resultado 
  correcto en ese lapso de tiempo. */
-var respuesta;
-var temporizador;
+let respuesta;
+let temporizador;
 function comenzar()
 {
-	
-}//FIN DE LA FUNCIÓN
+    num1 = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+    num2 = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+    operacion = Math.floor(Math.random() * (4 - 1 + 1)) + 1;
+ 
+     document.getElementById("txtIdPrimerNumero").value = num1;
+     document.getElementById("txtIDSegundoNumero").value = num2;
+ 
+     
+     if (operacion == 1){
+         operacion = "+";
+         document.getElementById("txtIdOperador").value = operacion; 
+        }
+        else if (operacion ==2){
+         operacion = "-";
+         document.getElementById("txtIdOperador").value = operacion; 
+        }
+        else if (operacion == 3){
+         operacion = "*";
+         document.getElementById("txtIdOperador").value = operacion; 
+        }
+        else if(operacion ==4) {
+         operacion = "/";
+         document.getElementById("txtIdOperador").value = operacion; 
+        }
+        else{
+         operacion = "error";
+        }
+}
 function Responder()
 {
 	
 
 
-}//FIN DE LA FUNCIÓN
+}

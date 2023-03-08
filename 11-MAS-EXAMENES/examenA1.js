077ac2f86c8a3ea9be5f89e27bab6ade50c549c9
franromeroe <franromeroeze@gmail.com>
2023-03-08 17:57:20 -0300

    parcial

---
+++ 11-MAS-EXAMENES/examenA1.js
@@ -0,0 +1,16 @@
+/*
+Romero Montero Franco Ezequiel
+division K
+turno Noche
+dni 40143483
+Ej 1
+
+
+
+*/
+
+
+function comenzar()
+{
+
+}
---
+++ 11-MAS-EXAMENES/examenA3.js
@@ -0,0 +1,15 @@
+/*
+Romero Montero Franco Ezequiel
+division K
+turno Noche
+dni 40143483
+Ej 1
+
+
+
+*/
+
+function comenzar()
+{
+
+}
